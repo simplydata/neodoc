1b4343bfa4e7507d867524cf7feb6df477f968a1
Felix Schlitter <felixschlitter@gmail.com>
2016-08-08 19:48:33 +1200

    WIP - simplify groups

--- src/Language/Docopt/Solver/Ungroup.purs
+++ src/Language/Docopt/Solver/Ungroup.purs
@@ -1,10 +1,31 @@
--- | Ungroup groups into their super groups if they only have one branch and all
--- | contained elements are optional.
+-- | Simplify groupings.
+-- |
+-- | This is somewhwat similar to the group simplification in the original
+-- | docopt except that this implementation will retain more (if not all) of
+-- | the semantics of the input.
+-- |
+-- | Example:
+-- |
+-- | [[-a]]          -> [-a]
+-- | [[-a] [-b]]     -> [-a -b]
+-- | [[-a] | [-b]]   -> [-a | -b]
+-- | [-a -b -c]      -> [-a] [-b] [-c]
+-- | [-a -b -c | -d] -> [-a -b -c | -d]
+-- |
+-- | Rules:
+-- | * Nested singleton group reduction (OR `on` `_.optional`):
+-- |    * ([...]) -> [...]
+-- |    * [(...)] -> [...]
+-- |    * [[...]] -> [...]
+-- |    * ((...)) -> (...)
+-- | * Non-singleton groups remain in-tact
+-- |    * [-a | -b] -> [-a | -b]
+-- |
 
 module Language.Docopt.Solver.Ungroup where
 
 import Prelude
-import Data.List (List(), singleton)
+import Data.List (List(Nil), singleton)
 import Language.Docopt
 
 unGroup :: Specification -> Specification
@@ -14,8 +35,19 @@ unGroup spec = do
     branch <- usage
     pure do
       arg <- branch
-      unGroupArg arg
+      unGroupArg topLevelContainer arg
 
-unGroupArg :: Argument -> List Argument
--- unGroupArg (Group { branches }) = 
-unGroupArg a = singleton a
+  where
+    topLevelContainer = { optional: false, repeatable: false }
+
+type Container = {
+  optional   :: Boolean
+, repeatable :: Boolean
+}
+
+-- | Simplify a single argument
+unGroupArg
+  :: Container -- ^ The containing argument
+  -> Argument  -- ^ The argument to simplify
+  -> List Argument
+unGroupArg _ _ = Nil
